5cb28a1327d2efbb8c2342e798819a22db96621b
Yash <yashwanth.venkatesan@gmail.com>
2026-03-07 17:55:25 -0500

    feat: implement theme context and header theme provider with theme selector

--- src/app/(payload)/layout.tsx
+++ src/app/(payload)/layout.tsx
@@ -1,12 +1,13 @@
 /* THIS FILE WAS GENERATED AUTOMATICALLY BY PAYLOAD. */
 /* DO NOT MODIFY IT BECAUSE IT COULD BE REWRITTEN AT ANY TIME. */
 import type { ServerFunctionClient } from 'payload'
-
+import '@payloadcms/next/css'
 import config from '@payload-config'
 import { handleServerFunctions, RootLayout } from '@payloadcms/next/layouts'
 import React from 'react'
 
 import { importMap } from './admin/importMap.js'
+import '@/styles/globals.css'
 import './custom.scss'
 
 type Args = {
